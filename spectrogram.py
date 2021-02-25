c8754bcdadd349d9f99c2986499c53a4131beca4
rwhennigan <73253397+rwhennigan@users.noreply.github.com>
2021-02-24 19:10:44 -0500

    Modified Galahad's example
    I used specgram to plot, and also changed the variable being plotted. Instead of plotting abs(data), I got the magnitude of data first:
    spec2 = abs(np.square(np.real(shifted_spectrum)))+abs(np.square(np.imag(shifted_spectrum)))

---
+++ spectrogram.py
@@ -0,0 +1,29 @@
+#imports
+import numpy as np
+import adi
+import matplotlib.pyplot as plt
+from scipy.fft import fftshift, fft
+from scipy import signal
+
+#configure pluto
+sample_rate = 8e6
+center_freq = 2.45e9
+exponent = 13
+num_samples = 2**exponent
+
+sdr = adi.Pluto ("ip:192.168.2.1")
+
+sdr.sample_rate = int(sample_rate)
+sdr.rx_rf_bandwidth = int(sample_rate)
+sdr.rx_lo = int(center_freq)
+sdr.rx_buffer_size = num_samples
+
+data = sdr.rx()
+shifted_spectrum = fftshift(fft(data))
+print(shifted_spectrum)
+spec2 = abs(np.square(np.real(shifted_spectrum)))+abs(np.square(np.imag(shifted_spectrum)))
+plt.figure(0)
+plt.specgram(abs(shifted_spectrum),scale='dB')
+plt.figure(1)
+plt.specgram(spec2,scale='dB')
+plt.show()
