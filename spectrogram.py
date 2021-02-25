d305f510d2bc897f92977e586e9d01b9746ba9b1
YossefN <yhnaim@wpi.edu>
2021-02-25 17:29:46 -0500

    Updated spectrogram.py: Continuous updating as well as updated sampling rate

--- spectrogram.py
+++ spectrogram.py
@@ -1,14 +1,15 @@
 #imports
 import numpy as np
 import adi
+import time
 import matplotlib.pyplot as plt
 from scipy.fft import fftshift, fft
 from scipy import signal
 
 #configure pluto
-sample_rate = 8e6
+sample_rate = 60e6
 center_freq = 2.45e9
-exponent = 13
+exponent = 10
 num_samples = 2**exponent
 
 sdr = adi.Pluto ("ip:192.168.2.1")
@@ -18,12 +19,17 @@ sdr.rx_rf_bandwidth = int(sample_rate)
 sdr.rx_lo = int(center_freq)
 sdr.rx_buffer_size = num_samples
 
-data = sdr.rx()
-shifted_spectrum = fftshift(fft(data))
-print(shifted_spectrum)
-spec2 = abs(np.square(np.real(shifted_spectrum)))+abs(np.square(np.imag(shifted_spectrum)))
-plt.figure(0)
-plt.specgram(abs(shifted_spectrum),scale='dB')
-plt.figure(1)
-plt.specgram(spec2,scale='dB')
+for x in range(15):
+    data = sdr.rx()
+    shifted_spectrum = fftshift(fft(data))
+    print(shifted_spectrum)
+    spec2 = abs(np.square(np.real(shifted_spectrum)))+abs(np.square(np.imag(shifted_spectrum)))
+    #plt.figure(0)
+    #plt.specgram(abs(shifted_spectrum),scale='dB')
+    #plt.figure(1)
+    plt.draw()
+    plt.pause(0.001)
+    time.sleep(0.001)
+    plt.specgram(spec2,scale='dB')
+
 plt.show()
