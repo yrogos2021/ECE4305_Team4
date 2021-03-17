c3a27c018d69b9a3af44ea02a196034acf7713ed
yrogos2021 <73254637+yrogos2021@users.noreply.github.com>
2021-03-16 21:12:28 -0400

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -1,4 +1,4 @@
-# ------------------------------------------
+## ------------------------------------------
 # Author: Yossef Naim
 # Institution: Worcester Polytechinc Institute
 # Date: 02/20/2021
@@ -17,10 +17,7 @@ import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 import scipy.special as special
 
-# Definitions
-#def integrand(x,a,b)
-#    return
-
+# --------- Radio Configuration ---------
 
 # Center and Sampling Frequencies
 sample_rate = 8e6
@@ -40,33 +37,18 @@ samplesRecieved = 0
 data = sdr.rx()
 
 # --------- Constants ---------
-#t = np.linspace(0.0, (N-1)/(float(sample_rate)),N)
-t = N*(1/sample_rate)
-
 deltaF = 0.0
-phi = angle(data)
+t = np.linspace(0.0, (num_samples-1)/(float(sample_rate)), num_samples)
+PhaseOffset = 0.0
 
-t = N*(1/sample_rate)
+data_phase = angle(data)
 
-deltaF = 0.0
-phi = angle(data)
+# --------- Ideal Signal ---------
+offset = 1e6
+Ideal_dataI = np.cos(2.0*np.pi*(offset+deltaF)*t+PhaseOffset*np.ones(num_samples))
+Ideal_dataQ = -np.sin(2.0*np.pi*(offset+deltaF)*t+PhaseOffset*np.ones(num_samples))
 
-#for loop from 0 to N-1
-e = []
-f_n = np.zeros(N)
-#for G
-B_L = 0.01
-damping = 1
-M = 2e6
-K = 1
-theta = B_L/(M*((damping+0.25)/damping))
-delta = 1+ (2*dampng *theta)+ (theta**2)
-G = (4*damping *theta/delta)/(M*K)
-loop_filter= np.empty(N, dtype=complex)
-loop_filter_past = 0.0
-e_past = 0.0
-
-# --------- Take Spectrum of Signal ---------
+# --------- Time to Frequency and Shifted ---------
 time_to_freq = numpy.fft(data)
 shifted_frequency = numpy.fft.fftshift(time_to_freq)
 energy_sum = sum(shifted_frequency)
@@ -87,14 +69,18 @@ bin_w = sample_rate/len(shifted_frequency)
 num_offset = sample_center - len(shifted_frequency)/2
 foffset = -bin_w * num_offset
 
-# ----------- Stuff From Chpt. 6 ------------
-time_axis = np.linspace( 0, num_samples/sample_rate, len(data))
+# ----------- Coarse Frequency Correction ------------
+time_axis = np.linspace(0, num_samples/sample_rate, len(data))
 neg_freq_detrend_line = np.exp(1j*2*np.pi*0.5*sample_rate*time_axis)
 coarse_freq_correct = np.exp(1j*2*np.pi*foffset*time_axis)
-center_data = data*neg*freq_detrend_line*coarse_freq_correct
 
+# ----------- Coarse Frequency Correction ------------
+center_data = data*neg_freq_detrend_line*coarse_freq_correct
+DC_centered_data = data*neg_freq_detrend_line
+
+unwrapped_data_phase_DC = np.unwrap(np.angle(DC_centered_data))
 unwrapped_data_phase = np.unwrap(np.angle(center_data))
-unwrapped_compensator_phase = np.wrap(compensator_phase)
+unwrapped_compensator_phase = np.unwrap(compensator_phase)
 
 data_phase_derivative = np.diff(unwrapped_data_phase)
 
@@ -102,29 +88,45 @@ shifted = data_phase_derivative**2
 shifted = list(map(lambda x: 0 if x < 4 else x, shifted))
 
 # ------------------ DPLL -------------------
+#for G
+B_L = 0.01
+damping = 1
+M = 2e6
+K = 1
+theta = B_L/(M*((damping+0.25)/damping))
+delta = 1+ (2*dampng *theta)+ (theta**2)
+G = (4*damping *theta/delta)/(M*K)
 
-for i in range(N):
+#for DPLL
+correction_output = np.empty(N, dtype = complex)
+e = np.empty(N, dtype = complex)
+f_n = np.zeros(N)
+loop_filter= np.empty(N, dtype=complex)
+loop_filter_past = 0.0
+e_past = 0.0
+
+for i in range(num_samples):
 	#phase rotate
 	if i==0:
-		gamma = data_phase - (2*np.pi*deltaF*t)
+		phase_rotator = centered_data[i] * deltaF
+		correction_output[i] = phase_rotator
 	else:
-		gamma = data_phase - (2*np.pi*new_delta*t)
-
-	# Error
-	if np.all(abs(2*np.pi*foffset*t - gamma[i]) > abs(2*np.pi*-foffset*t - gamma[i])):
-		e[i] = 2*np.pi*(-foffset*t) - gamma[i] # '0'
-	else:
-		e[i] = 2*np.pi*foffset*t - gamma[i] # '1'
+		phase_rotator = centered_data[i] * new_delta
+		correction_output[i] = phase_rotator	
+	# Errorf
+	e[i] = phase_rotator * (Ideal_dataI[i] +1j*Ideal_dataQ[i])
+	
 	loop_filter[i] = loop_filter_past + G * e_past
 	e_past = e[i]
 	loop_filter_past = loop_filter[i]
 
 	# DDS - this is if we use the summation format
 
-	f_n_angle = np.angle(f_n[i])
+	f_n_angle = np.angle(loop_filter[i])
 	new_delta= np.exp(-1j*f_n_angle)
 	
 # --------------- Frame Sync ----------------
 
 
 #----------------------------------------------------
+
